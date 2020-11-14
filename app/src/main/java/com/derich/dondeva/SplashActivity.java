9fc2915bbc835d6e0d7c64427dfcd0a53fce842b
AlanDerich <alangitonga15@gmail.com>
2020-11-14 12:31:38 +0300

    Initial commit

---
+++ app/src/main/java/com/derich/dondeva/SplashActivity.java
@@ -0,0 +1,39 @@
+package com.derich.dondeva;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.VideoView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    VideoView videoView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        videoView = findViewById(R.id.videoViewSplash);
+
+        Uri video = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.dondeva);
+        videoView.setVideoURI(video);
+
+        videoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            public void onCompletion(MediaPlayer mp) {
+                startNextActivity();
+            }
+        });
+
+        videoView.start();
+    }
+
+    private void startNextActivity() {
+        if (isFinishing())
+            return;
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
+    }
+}
